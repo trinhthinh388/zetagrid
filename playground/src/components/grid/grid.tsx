0debf0f45b059a572f13e92ef04047814e7a5321
thinh.trinhduc <thinh.trinh@covergo.com>
2026-05-31 21:28:15 +0700

    feat: add faker-js and integrate mock data generation into the grid component

--- playground/src/components/grid/grid.tsx
+++ playground/src/components/grid/grid.tsx
@@ -2,12 +2,17 @@ import { ScrollModule } from '@core/modules/scroll';
 import { SizeWatcherModule } from '@core/modules/size-watcher';
 import { VirtualizedModule } from '@core/modules/virtualized';
 import { Grid as ZetaGrid } from '@react';
+import { useState } from 'react';
 import { columnDefs } from './columns';
+import { generateMockData } from './mockData';
 
 export const Grid = () => {
+  const [data] = useState(() => generateMockData(100));
+
   return (
     <div className="w-[800px] h-[500px] mx-auto">
       <ZetaGrid
+        data={data}
         columnDefs={columnDefs}
         modules={[SizeWatcherModule, ScrollModule, VirtualizedModule]}
       />
